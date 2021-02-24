e2eebf3d6526c11c8332a9b83a05cd260263cc1e
Simon Chretien <simon.chretien62@gmail.com>
2021-02-24 18:32:15 +0100

    Plants are burnt

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Garden.destroy_all
+
+
+Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
